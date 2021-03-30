b694ba10c41bb66e1025e73331b5bb9d188b9fbc
Stanislau Ebo da Silva <acido2009@hotmail.com.br>
2021-03-30 09:33:39 -0400

    link-github

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <nav className="navbar navbar-light bg-dark">
-        <a className="navbar-brand text-danger" href={"/"}>Stani DEV</a>
+        <a className="navbar-brand text-danger" href={"https://github.com/Stanisds12"}>Stani DEV</a>
       </nav>
       <InputText />
      
